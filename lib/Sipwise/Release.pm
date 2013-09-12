f45ddc3887c092b796954be493e7c4890de92d60
Gerhard Jungwirth <gjungwirth@sipwise.com>
2013-09-12 16:46:30 +0200

    Add release documentation (tagging a repository)
    This has been moved from ngcp-schema
    (Add documentation about "Tagging a new release")

---
+++ lib/Sipwise/Release.pm
@@ -0,0 +1,41 @@
+package Sipwise::Release 1.004;
+use Sipwise::Base;
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+Sipwise::Release - Sipwise Release Documentation
+
+=head1 VERSION
+
+This document describes Sipwise::Release version 1.004
+
+=head1 TAGGING A NEW RELEASE
+
+=over
+
+=item Create a new entry in the Changes file
+
+=item Update debian/changelog using the command git-dch like this:
+C<git-dch --id-length=7 --meta --multimaint-merge --ignore-branch --release --auto>
+
+=item *
+
+Update the perl files using
+C<perl-reversion -dryrun -set $VERSION>. Remove C<-dryrun> to actually
+run this command.
+
+=item git commit your work with $VERSION as commit message
+
+=item run C<git tag -u $KEY -s $VERSION>
+
+=item run C<git push> and C<git push --tags>
+
+=back
+
+=head1 AUTHOR
+
+C<< <gjungwirth@sipwise.com> >>
